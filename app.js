31c5626a3a6164f814140b62893d3a3075db7c05
SanOP360 <sanjayl2001alm@gmail.com>
2024-02-29 22:55:29 +0530

    added express

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const http = require("http");
-const routes= require('./routes');
+const express=require('express');
 
-const server = http.createServer(routes);
+const app= express();
 
-server.listen(4000, () => {
-  console.log("Server is running on port 4000");
+app.use((req,res,next)=>{
+  console.log('In the middleware');
+  next();
 });
- 
+
+app.use((req, res, next) => {
+  console.log("In another middleware");
+  res.send('<h1>Hello from Express</h1>');
+});
+
+
+app.listen(3000);
+
+
+
