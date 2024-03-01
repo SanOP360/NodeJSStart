4b6e0c30bc2df6e389c64985c7a70741f06e1add
SanOP360 <sanjayl2001alm@gmail.com>
2024-03-01 12:15:40 +0530

    added middleware

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express=require('express');
+const boydParser = require("body-parser");
+
 
 const app= express();
 
-app.use((req,res,next)=>{
-  console.log('In the middleware');
+app.use(boydParser.urlencoded({extended:false}));
+
+app.use('/',(req,res,next)=>{
+  
   next();
+})
+app.use("/add-product", (req, res, next) => {
+  
+  res.send(
+    "<form action='/product' method='POST'><label>Name</label><input type='text' name='title'><br><label>Size</label> <input type='number' name='Size'><br><button type='submit'>Add</button></form>"
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("In another middleware");
+app.post('/product',(req,res,next)=>{
+  console.log(req.body);
+  res.redirect('/');
+})
+
+app.use('/',(req, res, next) => {
+  
   res.send('<h1>Hello from Express</h1>');
 });
 
